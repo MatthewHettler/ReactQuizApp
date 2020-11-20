b11fea6cecf5d0b0043171ac06fe17565d0feb5a
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-11-20 10:06:43 -0600

    Made first project commit.

---
+++ react-quiz-app/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+export default function App() {
+
+  const questions = [
+
+
+    {
+        questionText: 'What is the capital of France?',
+        answerOptions: [
+          { answerText: 'New York', isCorrect: false },
+          { answerText: 'London', isCorrect: false },
+          { answerText: 'Paris', isCorrect: true },
+          { answerText: 'Dublin', isCorrect: false },
+        ]
+    },
+
+
+
+  ]
+
+}
